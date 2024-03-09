c16cf4d6192fbda8ba1340428c6322172196a4a3
Harsha <harshareddy4471@gmail.com>
2024-03-09 14:33:40 +0530

    added both the edited files

--- src/main/java/com/cts/PracticeAppApplication.java
+++ src/main/java/com/cts/PracticeAppApplication.java
@@ -8,6 +8,7 @@ public class PracticeAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeAppApplication.class, args);
+		System.out.println("welcome D20");
 	}
 
 }
